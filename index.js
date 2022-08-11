3b97db7816b362055bce4df03434c0e6723b7392
Shane Meyers <smeyers1031@gmail.com>
2022-08-11 12:31:09 -0700

    commit tree 497ea4117342ac4e53b012a679ee08ae7584dd62

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "YOUR-NAME is the champion";
